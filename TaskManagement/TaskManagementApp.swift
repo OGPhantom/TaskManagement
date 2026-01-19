bcb36cbcd4651b0c639dc72eeffaef64b7d65701
OGPhantom <nikita.storchai@nure.ua>
2026-01-19 22:06:16 +0200

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Никита Сторчай on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
